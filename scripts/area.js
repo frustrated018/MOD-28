be6fcd244f5f39bff04e9cd55e32f627e4f8f72b
frustrated018 <niloyd018@gmail.com>
2023-08-18 12:55:44 +0600

    Made the pentagon card and improved the text area clearing function

--- scripts/area.js
+++ scripts/area.js
@@ -1,8 +1,10 @@
 // Clearing the input section
 
-function clearInputById(id) {
-  const inputElement = document.getElementById(id);
-  inputElement.value = "";
+function clearInputById(id1, id2) {
+  const id1Clear = document.getElementById(id1);
+  id1Clear.value = "";
+  const id2Clear = document.getElementById(id2);
+  id2Clear.value = "";
 }
 
 // Making a reusable function to get the height and base -->
@@ -15,27 +17,26 @@ function calculateTriangleArea() {
   const area = getInputValue("tri-base") * getInputValue("tri-height") * 0.5;
   // return area;
   console.log(area);
-  clearInputById("tri-base");
-  clearInputById("tri-height");
+  clearInputById('tri-base', 'tri-height');
 }
 function calculateRectangleArea() {
   const area = getInputValue("rect-width") * getInputValue("rect-height");
   console.log(area);
 
-  clearInputById("rect-width");
-  clearInputById("rect-height");
+  clearInputById('rect-width', 'rect-height');
 }
 function calculateParallelogramArea() {
   const area = getInputValue("para-base") * getInputValue("para-height");
   console.log(area);
-
-  clearInputById("para-base");
-  clearInputById("para-height");
+  clearInputById('para-base', 'para-height');
 }
 function calculateRhombusArea() {
-  const area = getInputValue("diagonal-1") * getInputValue("diagonal-2");
+  const area = getInputValue("diagonal-1") * getInputValue("diagonal-2") * .5;
   console.log(area);
-
-  clearInputById("diagonal-1");
-  clearInputById("diagonal-2");
+  clearInputById('diagonal-1', 'diagonal-2');
+}
+function calculatePentagonArea() {
+  const area = getInputValue("perimeter") * getInputValue("apothem") * .5;
+  console.log(area);
+  clearInputById('perimeter', 'apothem');
 }
