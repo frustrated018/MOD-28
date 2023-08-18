e02df0ab4d51cafe585f40844670d3f4017187a9
frustrated018 <niloyd018@gmail.com>
2023-08-18 15:35:06 +0600

    Made a validation function.

--- scripts/area.js
+++ scripts/area.js
@@ -7,41 +7,57 @@ function clearInputById(id1, id2) {
   id2Clear.value = "";
 }
 
-// Making a reusable function to get the height and base -->
+// Making a reusable function to get the height and base and other inputs -->
 function getInputValue(id) {
   const value = parseFloat(document.getElementById(id).value);
   return value;
 }
 
+// Function for validation
+
+function validate(area) {
+  if (isNaN(area) === true) {
+    alert("Please insert a valid number");
+    return;
+  }
+  // else{
+  //     console.log(area);  already using console.log inside the individual functions
+  // }
+}
+
 function calculateTriangleArea() {
   const area = getInputValue("tri-base") * getInputValue("tri-height") * 0.5;
-  // return area;
+  validate(area);
   console.log(area);
-  clearInputById('tri-base', 'tri-height');
+  clearInputById("tri-base", "tri-height");
 }
 function calculateRectangleArea() {
   const area = getInputValue("rect-width") * getInputValue("rect-height");
+  validate(area);
   console.log(area);
-
-  clearInputById('rect-width', 'rect-height');
+  clearInputById("rect-width", "rect-height");
 }
 function calculateParallelogramArea() {
   const area = getInputValue("para-base") * getInputValue("para-height");
+  validate(area);
   console.log(area);
-  clearInputById('para-base', 'para-height');
+  clearInputById("para-base", "para-height");
 }
 function calculateRhombusArea() {
-  const area = getInputValue("diagonal-1") * getInputValue("diagonal-2") * .5;
+  const area = getInputValue("diagonal-1") * getInputValue("diagonal-2") * 0.5;
+  validate(area);
   console.log(area);
-  clearInputById('diagonal-1', 'diagonal-2');
+  clearInputById("diagonal-1", "diagonal-2");
 }
 function calculatePentagonArea() {
-  const area = getInputValue("perimeter") * getInputValue("apothem") * .5;
+  const area = getInputValue("perimeter") * getInputValue("apothem") * 0.5;
+  validate(area);
   console.log(area);
-  clearInputById('perimeter', 'apothem');
+  clearInputById("perimeter", "apothem");
 }
 function calculateEllipseArea() {
   const area = getInputValue("el-major") * getInputValue("el-minor") * Math.PI;
-  console.log(area);
-  clearInputById('el-major', 'el-minor');
+  validate(area);
+  console.log(area.toFixed(3));
+  clearInputById("el-major", "el-minor");
 }
