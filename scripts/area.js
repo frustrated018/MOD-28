76846a4bf4f0ea0c164a572cd0f32d292d359c47
frustrated018 <niloyd018@gmail.com>
2023-08-18 20:37:00 +0600

    Made a function that stores values in the area calc section. But the button inside that needs some work

--- scripts/area.js
+++ scripts/area.js
@@ -24,40 +24,71 @@ function validate(area) {
   //     console.log(area);  already using console.log inside the individual functions
   // }
 }
+// Adding to calculation entry
+function addToCalculationEntry(entryName, value) {
+  const entryPoint = document.getElementById("calculation-entry");
+  const entry = document.createElement("div");
+  entry.className = "flex gap-3 items-center justify-evenly my-4";
+  entry.innerHTML = `<p class = "text-neutral-700 text-base font-normal">
+    ${
+      entryPoint.childElementCount + 1
+    }. ${entryName} <span id = "cm-value">${value} cm</span><sup>2</sup></p>
+    <button id="btn-convert" onclick="convert(${value})"class=" w-24 h-8 bg-sky-600 rounded text-xs text-white">Convert to m<sup>2</sup></button>`;
+  entryPoint.appendChild(entry);
+  if(convertBtn){
+      convertBtn.removeEventListener('click', convert);
+    }
+   let convertBtn = document.getElementById("btn-convert");
+  convertBtn.addEventListener('click', convert);
+}
+// convert button function
+function convert(valueInCm) {
+//   const valueInCm = parseFloat(document.getElementById("cm-value").innerText);
+   const valueInM = Math.pow(valueInCm, 2);
+   const cmValueSpan = document.getElementById("cm-value");
+   cmValueSpan.innerText= `${valueInM} m`;
+   return;
+}
 
+/************************* Triangle **************************/
 function calculateTriangleArea() {
   const area = getInputValue("tri-base") * getInputValue("tri-height") * 0.5;
-  validate(area);
-  console.log(area);
   clearInputById("tri-base", "tri-height");
+  validate(area);
+  addToCalculationEntry("Triangle", area);
 }
+/************************* Rectangle **************************/
 function calculateRectangleArea() {
   const area = getInputValue("rect-width") * getInputValue("rect-height");
-  validate(area);
-  console.log(area);
   clearInputById("rect-width", "rect-height");
+  validate(area);
+  addToCalculationEntry("Rectangle", area);
 }
+/************************* Parallelogram **************************/
 function calculateParallelogramArea() {
   const area = getInputValue("para-base") * getInputValue("para-height");
-  validate(area);
-  console.log(area);
   clearInputById("para-base", "para-height");
+  validate(area);
+  addToCalculationEntry("Parallelogram", area);
 }
+/************************* Rhombus **************************/
 function calculateRhombusArea() {
   const area = getInputValue("diagonal-1") * getInputValue("diagonal-2") * 0.5;
-  validate(area);
-  console.log(area);
   clearInputById("diagonal-1", "diagonal-2");
+  validate(area);
+  addToCalculationEntry("Rombus", area);
 }
+/************************* Pentagon **************************/
 function calculatePentagonArea() {
   const area = getInputValue("perimeter") * getInputValue("apothem") * 0.5;
-  validate(area);
-  console.log(area);
   clearInputById("perimeter", "apothem");
+  validate(area);
+  addToCalculationEntry("Pentagon", area);
 }
+/************************* Ellipse **************************/
 function calculateEllipseArea() {
   const area = getInputValue("el-major") * getInputValue("el-minor") * Math.PI;
-  validate(area);
-  console.log(area.toFixed(3));
   clearInputById("el-major", "el-minor");
+  validate(area);
+  addToCalculationEntry("Ellipse", area.toFixed(3));
 }
